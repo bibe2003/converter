becfe36ea23c4a956017d21b3bcce36fd844b68d
Biljana Miloshevska <biljana.miloshevska1@smail.astate.edu>
2014-04-24 16:18:36 -0500

    GUI created

--- Converter/src/javaapplication2/Panel.java
+++ Converter/src/javaapplication2/Panel.java
@@ -8,6 +8,7 @@ package javaapplication2;
 
 	import java.awt.*;
 	import java.awt.event.*;
+import java.text.AttributedString;
 	import javax.swing.*;
 	
 	/*************************************************************
@@ -21,27 +22,32 @@ package javaapplication2;
 	  private JButton convertButton;
           JMenu m = new JMenu("Color");
           public static JMenuBar menuBar;
-          public static JMenuItem menuItem;
-          public static JMenuItem menuItem2;
-          public static JMenuItem menuItem3;
+          public static JMenuItem about;
+          public static JMenuItem update;
+          public static JMenuItem exit;
           public static JMenu menu;
           public static JTextField convertFromTextField;
           public static JTextField convertToTextField;
-          
-         
+          public String info = "\n          Currency Converter™ "
+                  + "\n\nDeveloped by Biljana Miloshevska            "
+                  + "\n\n      Arkansas State University\n\n";
           
           public Panel ()
           {
             // first create font for text fields
             Font f1 = new Font("Helvetica", Font.ITALIC, 14);
             
+            /************************************************
+            ** Text Field for entering base currency value **
+            ************************************************/
             add(new JLabel("United States Dollar "));
             convertFromTextField = new JTextField("                ");
             convertFromTextField.setFont(f1); // set font
-            convertFromTextField.setAlignmentY(TOP_ALIGNMENT);
-            convertFromTextField.setAlignmentX(LEFT_ALIGNMENT);
             add(convertFromTextField);
             
+            /**********************************************
+            ** Pull down menu for picking base currency  **
+            **********************************************/
             DefaultComboBoxModel model = new DefaultComboBoxModel();
             // use flex JSON for these
             model.addElement("USD");
@@ -50,130 +56,121 @@ package javaapplication2;
             JComboBox comboBox = new JComboBox(model);
             add(comboBox);
             
-            add(new JLabel("       Canadian Dollar       "));
-              
+            /****************************************************
+            ** Text field for entering the base currency value **
+            ****************************************************/
+            add(new JLabel("Canadian Dollar"));
             convertToTextField = new JTextField("                ");
             convertToTextField.setFont(f1);
-            convertToTextField.setAlignmentY(TOP_ALIGNMENT);
-            convertToTextField.setAlignmentX(LEFT_ALIGNMENT);
-            // convertToTextField.setPrefferedSize;
-            convertToTextField.setAlignmentY(BOTTOM_ALIGNMENT);
-            convertToTextField.setAlignmentX(BOTTOM_ALIGNMENT);
             convertToTextField.disable();
-          //  convertToTextField.isShowing();
             add(convertToTextField);
             
+            /****************************************************
+            ** Pull down menu for currency to which we convert **
+            ****************************************************/
             DefaultComboBoxModel model2 = new DefaultComboBoxModel();
-            model2.addElement("CND");
+            model2.addElement("CND"); 
             model2.addElement("Euro");
             model2.addElement("Yen");
             JComboBox comboBox2 = new JComboBox(model2);
             add(comboBox2);
-           
-            add(new JLabel("Exchange rate is: 1.5 "));
             
-             /* CREATE CONVERT BUTTON */  
+            add(new JLabel("Exchange rate is: 1.5 ")); // print out exchange rate
+            
+            /****************************************************
+            **                 Convert Button                  **
+            ****************************************************/ 
 	    convertButton = new JButton("CONVERT"); 
 	    add(convertButton, BorderLayout.CENTER);  // add button to current panel
-	    // register the current panel as listener for the buttons
-            convertButton.setActionCommand("Convert");
-	    convertButton.addActionListener(this); 
+            convertButton.setActionCommand("Convert"); // name of the event action
+	    convertButton.addActionListener(this); // listen for an event (click)
 
-            /* CREATE MENU */
+            /****************************************************
+            **                 Create Menu                     **
+            ****************************************************/ 
             menu = new JMenu("Menu");
             menu.setMnemonic(KeyEvent.VK_A);
             menu.getAccessibleContext().setAccessibleDescription("Menu");
-            menuBar.add(menu);
+            menuBar.add(menu); // add menu to panel
             
-            //a group of JMenuItems
+            /**************************************************
+            **         Create a group of JMenu items         **
+            **************************************************/ 
+            /* menu about                                                **
+            ** - dialog box containing: App title, your name, "OK" button*/
+            about = new JMenuItem("About",KeyEvent.VK_A);
+            about.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.ALT_MASK));
+            about.getAccessibleContext().setAccessibleDescription("About");
+            about.setActionCommand("About");
+            menu.add(about); // add "About" to menu
+            /* menu Update                                               **
+            ** - Read RSS feed for FromCurrency                          **
+            ** - Build HashMap "ToCurrency/FromCurrency to Exchange Rate */
+            update = new JMenuItem("Update",KeyEvent.VK_U);
+            update.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_U, ActionEvent.ALT_MASK));
+            update.getAccessibleContext().setAccessibleDescription("Update");
+            update.setActionCommand("Update");
+            menu.add(update); // add "Update" to menu
+            /* menu Exit                                                  **
+            ** - Clean Up application
+            ** - Exit Application                                         */
+            exit = new JMenuItem("Exit",KeyEvent.VK_E);
+            exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.ALT_MASK));
+            exit.getAccessibleContext().setAccessibleDescription("Exit application");
+            exit.setActionCommand("Exit"); // command called Exit
+            menu.add(exit); // add "Exit" to menu
             
-            /* menu about 
-               - dialog box containing: App title, your name, "OK" button*/
-            menuItem = new JMenuItem("About",
-                                     KeyEvent.VK_A);
-            menuItem.setAccelerator(KeyStroke.getKeyStroke(
-                    KeyEvent.VK_A, ActionEvent.ALT_MASK));
-            menuItem.getAccessibleContext().setAccessibleDescription("About");
-            menuItem.setActionCommand("About");
-            menu.add(menuItem); // add "About" to menu
-            /* menu Update 
-               - Read RSS feed for FromCurrency
-               - Build HashMap "ToCurrency/FromCurrency to Exchange Rate */
-            menuItem2 = new JMenuItem("Update",
-                                     KeyEvent.VK_U);
-            menuItem2.setAccelerator(KeyStroke.getKeyStroke(
-                    KeyEvent.VK_U, ActionEvent.ALT_MASK));
-            menuItem2.getAccessibleContext().setAccessibleDescription("Update");
-            menuItem2.setActionCommand("Update");
-            menu.add(menuItem2); // add "Update" to menu
-            /* menu Exit 
-               - Clean Up application
-               - Exit Application*/
-            menuItem3 = new JMenuItem("Exit",KeyEvent.VK_E);
-            menuItem3.setAccelerator(KeyStroke.getKeyStroke(
-                    KeyEvent.VK_E, ActionEvent.ALT_MASK));
-            menuItem3.getAccessibleContext().setAccessibleDescription("Exit application");
-            menuItem3.setActionCommand("Exit"); // command called Exit
-            menu.add(menuItem3); // add "Exit" to menu
-            
-            // action listener for each menu item
-            menuItem.addActionListener(this);
-            menuItem2.addActionListener(this);
-            menuItem3.addActionListener(this);
+            /**************************************************
+            **      Action listener for each menu item       **
+            **************************************************/ 
+            about.addActionListener(this);
+            update.addActionListener(this);
+            exit.addActionListener(this);
           }
           
-	 /*   public void paintComponent(Graphics g) {
-	    super.paintComponent(g);
-	    Font f = new Font("Helvetica", Font.BOLD, 16);
-	    Font fi = new Font("Helvetica", Font.BOLD + Font.ITALIC, 16);
-	    FontMetrics fm = g.getFontMetrics(f);
-	    FontMetrics fim = g.getFontMetrics(fi);
-	    int cx = 20; int cy = 200;
-	    g.setFont(f);
-            
-	    g.drawString("Converted:", cx, cy);
-            
-            
-	   /* for italics
-            cx += fm.stringWidth("Enter ");
-	    g.setFont(fi);
-	    g.drawString("currency:", cx, cy);
-            
-	  } //paintComponent*/
-
-
 	  //=============================================
-	  ///////////// main ////////////////////////////
+	  //////////////////// main /////////////////////
 
 	  public static void main(String[] args) {
             // create a JFrame instance of our interface
 	    JFrame f = new Interface("Currency Converter");
-            // show interface
-	    f.show();
+	    f.show();  // show interface
 	  } //main
-
-          /* Function onExit () no argument, void return value
-             - Cleans Up application
-             - Exits Application safely */
+          
+          /******************************************************
+          ** Function onExit () no argument, void return value **
+          ** - Cleans Up application                           **
+          ** - Exits Application safely                        **
+          *******************************************************/ 
           public void onExit ()
           {
-                System.exit(0);
+              // clean up application here
+                System.exit(0); // exit
           }
 
+          /******************************************************
+          ** Get action performed from menu items and call     **
+          ** appropriate functions.                            **
+          *******************************************************/ 
           @Override
           public void actionPerformed(ActionEvent e) {
-              if ("Convert".equals(e.getActionCommand())) { // if Convert pressed
+              // if Convert pressed
+              if ("Convert".equals(e.getActionCommand())) { 
                   
               }
-              else if ("Update".equals(e.getActionCommand())) // if Update pressed
+              // if Update pressed
+              else if ("Update".equals(e.getActionCommand())) 
               {
                   
               }
-              else if ("About".equals(e.getActionCommand())) // if About pressed
+              // if About pressed
+              else if ("About".equals(e.getActionCommand())) 
               {
-                  //Interface.aboutDialog();
+                  // show about dialog 
+                  JOptionPane.showMessageDialog(null, info, "About", 1);
               }
-              else // if user pressed Exit from menu
+              // if Exit pressed
+              else 
               {
                   onExit(); // calls onExit()
               }
